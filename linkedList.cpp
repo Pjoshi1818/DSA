183027814d7e5e8287990299ff417f5a7bd4e14a
Pjoshi1818 <pankajmenariyajoshi@gmail.com>
2025-07-06 15:59:04 +0530

    linked list

--- linkedList.cpp
+++ linkedList.cpp
@@ -1,65 +1,103 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Node {
+class Node
+{
 public:
-int data;
-Node* next;//point to the next node
+    int data;
+    Node *next; // point to the next node
 
-    Node(int val){
+    Node(int val)
+    {
         data = val;
         next = NULL;
     }
-
 };
 
-class list{
+class list
+{
 
-    Node* head;
-    Node * tail;
+    Node *head;
+    Node *tail;
 
-    public:
-    list(){
+public:
+    list()
+    {
         head = tail = NULL;
     }
 
-            void push_front(int val){
-                 Node* newnode = new Node(val);  // we use dynamic 
-                // Node newnode(val);  // static  {"It will get deleted as soon as it goes out of the push front, so we will not use it.}
-             
-                    if(head ==NULL){
-                        head = tail = newnode;
-                        return;
-                    }
-                        else{
-                            newnode->next = head;
-                            head =newnode;
-                        }
+    void push_front(int val)
+    {
+        Node *newnode = new Node(val); // we use dynamic //Because inside the list class, you create Node objects dynamically using:
+                                       // Node newnode(val);  // static  {"It will get deleted as soon as it goes out of the push front, so we will not use it.}
 
-            }
+        if (head == NULL)
+        {
+            head = tail = newnode;
+            return;
+        }
+        else
+        {
+            newnode->next = head;
+            head = newnode;
+        }
+    }
 
-            void printll(){
-                Node* temp = head;
-                while(temp != NULL){
-                    cout<<temp->data<<" ";
-                    temp = temp->next;
 
-                } 
-                cout<<endl;
-            }
 
+
+
+  void push_end(int val)
+    {
+        Node *newnode = new Node(val);
+
+        if (head == NULL)
+        {
+            head = tail = newnode;
+            return;
+        }
+        else
+        {
+            tail->next = newnode;
+            tail = newnode;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    void printll()
+    {
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
 };
 
-int main(){
-list ll;
+int main()
+{
+    list ll;
 
-ll.push_front(1);
-ll.push_front(2);
-ll.push_front(6);
+    ll.push_front(1);
+    ll.push_front(2);
+    ll.push_front(6);
 
-ll.printll();
+
+    ll.push_end(456);
+    ll.printll();
     return 0;
-
-
-
 }
+
+
