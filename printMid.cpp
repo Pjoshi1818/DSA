c95281d0510e13746e5cf2cd23adde1bd270d690
Pjoshi1818 <pankajmenariyajoshi@gmail.com>
2025-07-12 15:34:49 +0530

    middle of a linked list

--- printMid.cpp
+++ printMid.cpp
@@ -1,75 +1,86 @@
 #include <iostream>
 using namespace std;
 
-// Define the Node structure
-class Node {
+class node
+{
 public:
     int data;
-    Node* next;
+    node *next;
 
-    Node(int val) {
+    node(int val)
+    {
         data = val;
         next = NULL;
     }
 };
 
-// Define the Linked List class
-class LinkedList {
+class LinkedList
+{
 private:
-    Node* head;
+    node *head;
 
 public:
-    LinkedList() {
+    LinkedList()
+    {
         head = NULL;
     }
-
-    // Function to insert at the end
-    void insert(int val) {
-        Node* newNode = new Node(val);
-        if (head == NULL) {
-            head = newNode;
+    void insert(int val)
+    {
+        node *newnode = new node(val);
+        if (head == NULL)
+        {
+            head = newnode;
             return;
         }
 
-        Node* temp = head;
+        node *temp = head;
         while (temp->next != NULL)
             temp = temp->next;
 
-        temp->next = newNode;
+        temp->next = newnode;
     }
 
-    // Function to print the list
-    void display() {
-        Node* temp = head;
-        while (temp != NULL) {
+    void display()
+    {
+        node *temp = head;
+        while (temp != NULL)
+        {
             cout << temp->data << " -> ";
             temp = temp->next;
         }
         cout << "NULL" << endl;
     }
 
-    // Function to find and print the middle node
-    void printMiddle() {
-        if (head == NULL) {
-            cout << "List is empty!" << endl;
-            return;
+    void printMiddle()
+    {
+
+        node *temp = head;
+        int sz = 0;
+        while (temp != NULL)
+        {
+
+            temp = temp->next;
+
+            sz++;
+        }
+        int mid = sz / 2;
+        temp = head;
+
+        for (int i = 0; i < mid; i++)
+        {
+            temp = temp->next;
         }
 
-        Node* slow = head;
-        Node* fast = head;
-
-        // Move slow by 1 and fast by 2
-        while (fast != NULL && fast->next != NULL) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-
-        cout << "Middle element is: " << slow->data << endl;
+        if (temp != NULL)
+            cout << "Middle element is: " << temp->data << endl;
+        else
+            cout << "List is empty." << endl;
     }
 };
 
 // Main function
-int main() {
+int main()
+{
     LinkedList list;
     int n, val;
 
@@ -77,7 +88,8 @@ int main() {
     cin >> n;
 
     cout << "Enter elements:" << endl;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> val;
         list.insert(val);
     }
