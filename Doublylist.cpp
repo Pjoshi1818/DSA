71887f853fe8734df6ae59f045fa04b1ecda4f62
Pjoshi1818 <pankajmenariyajoshi@gmail.com>
2025-07-14 21:12:44 +0530

    doubly linked list

--- Doublylist.cpp
+++ Doublylist.cpp
@@ -1,53 +1,81 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-    class node {
-            public:
-            int data;
-            node* next;
-            node* prev;
-            node(int val){
-                data = val;
-            prev=next =NULL;
+class node
+{
+public:
+    int data;
+    node *next;
+    node *prev;
+    node(int val)
+    {
+        data = val;
+        prev = next = NULL;
+    }
+};
+class d_LIST
+{
+    node *head;
+    node *tail;
 
-            }
-    };
-    class   d_LIST{
-            node* head;
-            node* tail;
-            public:
-            d_LIST(){
-                head = tail =NULL;}        
-                
-                   void fush_front( int val){
-                    node* newnode = new node(val);
-                    if (head == NULL){
-                        head = newnode;
-                    }
-                    else {
-                        newnode->next= head;
-                        head->prev =newnode;
-                        head = newnode;
-                    }
-                   }
+public:
+    d_LIST()
+    {
+        head = tail = NULL;
+    }
 
-            
+    void push_front(int val)
+    {
+        node *newnode = new node(val);
+        if (head == NULL)
+        {
+            head = tail = newnode;
+        }
+        else
+        {
+            newnode->next = head;
+            head->prev = newnode;
+            head = newnode;
+        }
+    }
 
-                   void print(){
-                    node*temp;
-                    temp= head;
-                    while(temp != NULL){
-                             cout<<temp->data;
-                        temp=temp->next;
-                   
-                    }
-                   }
+    void push_back(int val)
+    {
+        node *newnode = new node(val);
 
-            };
-int main (){
+        if (head == NULL)
+        {
+            head = tail = newnode;
+            return;
+        }
+        else
+        {
+            newnode->prev= tail;
+            tail->next = newnode;
+            tail = newnode;
+        }
+    }
+
+    void print()
+    {
+        node *temp;
+        temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->data << " <--> ";
+            temp = temp->next;
+        }
+        cout << "NULL";
+    }
+};
+int main()
+{
 
     d_LIST dll;
-    dll.fush_front(52);
+       dll.push_front(500);
+    dll.push_back(52);
+    dll.push_front(500);
+    dll.push_back(852);
     dll.print();
     return 0;
 }
