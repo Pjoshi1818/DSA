a02d0da4a1462427f2cb2fa9ecfa915794f1f94c
Pjoshi1818 <pankajmenariyajoshi@gmail.com>
2025-07-18 23:28:51 +0530

    binary search using recursion:

---
+++ Binary_search_problem.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int bisearch(vector<int>& arr, int tar, int st, int end) {
+        if (st <= end) {
+            int mid = st + (end - st) / 2;
+
+            if (arr[mid] == tar) return mid;
+
+            else if (arr[mid] < tar) {
+                return bisearch(arr, tar, mid + 1, end);
+            }
+            else {
+                return bisearch(arr, tar, st, mid - 1);
+            }
+        }
+        return -1;
+    }
+
+    int search(vector<int>& arr, int tar) {
+        int st = 0, end = arr.size() - 1;
+        return bisearch(arr, tar, st, end);
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> arr = {1, 3, 5, 7, 9, 11, 13};
+    int target = 7;
+
+    int result = sol.search(arr, target);
+
+    if (result != -1) {
+        cout << " found at : " << result << endl;
+    } else {
+        cout << " not found." << endl;
+    }
+
+    return 0;
+}
--- arrIS_sorted.cpp
+++ arrIS_sorted.cpp
@@ -25,6 +25,7 @@ int main(){
             for(int i = 0 ; i<4;i++){
                 if(arr[i]>arr[i+1]){
                         flag = 1;
+                 
                         break;
                 }
              
