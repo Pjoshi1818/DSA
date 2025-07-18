999b08660692fcf0cd80e6976eed9caeba40943d
Pjoshi1818 <pankajmenariyajoshi@gmail.com>
2025-07-18 21:14:40 +0530

    nth fibonacci using recursion

--- Recursion.cpp
+++ Recursion.cpp
@@ -19,17 +19,36 @@
 // factorial 
 
 
-#include<iostream>
-using namespace std;
-int fec(int n){
-        if(n==0){
-            return 1;   
-        }
-        return n* fec(n-1);
-}
-int main(){
-int n ;
-n=4;
-cout<<fec(n)<<"  ";
-    return 0;
-}
+// #include<iostream>
+// using namespace std;
+// int fec(int n){
+//         if(n==0){
+//             return 1;   
+//         }
+//         return n* fec(n-1);
+// }
+// int main(){
+// int n ;
+// n=4;
+// cout<<fec(n)<<"  ";
+//     return 0;
+// }
+
+
+
+                                 // fibonacci
+
+
+                                 #include<iostream>
+                                 using namespace std;
+                                 int fibo(int n ){
+                                    if(n==0||n==1)
+                                    return n;
+
+                                    return fibo(n-1) + fibo(n-2);
+                                 }
+
+                                 int main(){
+                                   cout<< fibo(7);
+                                    return 0;
+                                 }
