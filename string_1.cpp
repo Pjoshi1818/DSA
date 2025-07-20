ce97b1a00175497ff261710bb839dd50d1960978
Pjoshi1818 <pankajmenariyajoshi@gmail.com>
2025-07-20 22:33:07 +0530

    string

--- string_1.cpp
+++ string_1.cpp
@@ -1,9 +1,37 @@
+
+        //cahracter arr 
+
+// #include<iostream>
+// #include<cstring>
+// using namespace std;
+// int main(){
+//         // char str[] = {'a','b','c','\0'};
+//         char str[]= "hello";
+//         cout<<strlen(str)<<endl;
+
+//         cout<<str<<endl;
+//         cout<<str[6]<<endl;
+
+//     return 0;
+// }
+
+
+
+            // string ***
+
 #include<iostream>
 #include<cstring>
 using namespace std;
 int main(){
-        char str[] = {'a','b','c','\0'};
-        cout<<strlen(str)<<endl;
+    string ch ; 
+
+    //string is a class in C++ (from the <string> header),
+    // and when we declare a variable of type string, we're basically creating an object of the string class.
+
+    
+    getline(cin ,ch);
+    cout<<ch;
 
     return 0;
+
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,9 +1,31 @@
+
+        //cahracter arr 
+
+// #include<iostream>
+// #include<cstring>
+// using namespace std;
+// int main(){
+//         // char str[] = {'a','b','c','\0'};
+//         char str[]= "hello";
+//         cout<<strlen(str)<<endl;
+
+//         cout<<str<<endl;
+//         cout<<str[6]<<endl;
+
+//     return 0;
+// }
+
+
+
+            // string ***
+
 #include<iostream>
-#include<cstring>
 using namespace std;
 int main(){
-        char str[] = {'a','b','c','\0'};
-        cout<<strlen(str)<<endl;
-
+    string ch ;
+    ch = "pankaj joshi ";
+    cout<<ch;
+    
     return 0;
+
 }
