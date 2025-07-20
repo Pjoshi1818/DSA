12ed52057264640cf6e3da928e136b436578729b
Pjoshi1818 <pankajmenariyajoshi@gmail.com>
2025-07-20 13:45:02 +0530

    pointer basic to ad. all opreaction

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -9,16 +9,35 @@
 // }
 
 
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     int a = 91;          // ASCII value
+//     int* ptr = &a;       // Pointer to a
+
+//     char ch = *ptr;      // Convert value at pointer to char
+//     cout << "Value: " << *ptr << endl;
+//     cout << "Character: " << ch << endl;
+
+//     return 0;
+// }
+
+
+
+// non pointer 
+
 #include<iostream>
 using namespace std;
 
 int main() {
-    int a = 91;          // ASCII value
-    int* ptr = &a;       // Pointer to a
+    char ch;
+    cout << "Enter a character: ";
+    cin >> ch;
 
-    char ch = *ptr;      // Convert value at pointer to char
-    cout << "Value: " << *ptr << endl;
+    int ascii = ch;  // Implicitly converts char to ASCII integer
     cout << "Character: " << ch << endl;
+    cout << "ASCII value: " << ascii << endl;
 
     return 0;
 }
