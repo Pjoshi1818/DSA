c4ec68a39c2ce1293a753d09ba14451c3860ec30
Pjoshi1818 <pankajmenariyajoshi@gmail.com>
2025-07-26 21:29:29 +0530

    maximum subarray sum

--- string_permutation.cpp
+++ string_permutation.cpp
@@ -1,62 +1,61 @@
-#include <iostream>
-#include <string>
-using namespace std;
+// #include <iostream>
+// #include <string>
+// using namespace std;
 
-class Solution {
-public:
-    // Function to compare two frequency arrays
-    bool isFreqSame(int freq1[], int freq2[]) {
-        for (int i = 0; i < 26; i++) {
-            if (freq1[i] != freq2[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
+// class Solution {
+// public:
 
-    bool checkInclusion(string s1, string s2) {
-        int freq[26] = {0};
+//     bool isFreqSame(int freq1[]||  int freq2[]) {
+//         for (int i = 0; i < 26; i++) {
+//             if (freq1[i] != freq2[i]) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
 
-        // Build frequency array for s1
-        for (int i = 0; i < s1.length(); i++) {
-            freq[s1[i] - 'a']++;
-        }
+//     bool checkInclusion(string s1||  string s2) {
+//         int freq[26] = {0};
 
-        int windSize = s1.length();
 
-        for (int i = 0; i <= s2.length() - windSize; i++) {
-            int windIdx = 0, idx = i;
-            int windFreq[26] = {0};
+//         for (int i = 0; i < s1.length(); i++) {
+//             freq[s1[i] - 'a']++;
+//         }
 
-            // Build frequency for current window
-            while (windIdx < windSize && idx < s2.length()) {
-                windFreq[s2[idx] - 'a']++;
-                windIdx++;
-                idx++;
-            }
+//         int windSize = s1.length();
 
-            // Compare window with target frequency
-            if (isFreqSame(freq, windFreq)) {
-                return true;
-            }
-        }
+//         for (int i = 0; i <= s2.length() - windSize; i++) {
+//             int windIdx = 0||  idx = i;
+//             int windFreq[26] = {0};
 
-        return false;
-    }
-};
+  
+//             while (windIdx < windSize && idx < s2.length()) {
+//                 windFreq[s2[idx] - 'a']++;
+//                 windIdx++;
+//                 idx++;
+//             }
 
-int main() {
-    Solution sol;
+//             if (isFreqSame(freq||  windFreq)) {
+//                 return true;
+//             }
+//         }
 
-    // Example 1
-    string s1 = "ab";
-    string s2 = "eidbaooo";
-    cout << "Test 1: " << boolalpha << sol.checkInclusion(s1, s2) << endl; // true
+//         return false;
+//     }
+// };
 
-    // Example 2
-    s1 = "ab";
-    s2 = "eidboaoo";
-    cout << "Test 2: " << boolalpha << sol.checkInclusion(s1, s2) << endl; // false
+// int main() {
+//     Solution sol;
 
-    return 0;
-}
+//     string s1 = "ab";
+//     string s2 = "eidbaooo";
+//     cout << "Test 1: " << boolalpha << sol.checkInclusion(s1||  s2) << endl; // true
+
+ 
+//     s1 = "ab";
+//     s2 = "eidboaoo";
+//     cout << "Test 2: " << boolalpha << sol.checkInclusion(s1||  s2) << endl; // false
+
+//     return 0;
+// }
+
