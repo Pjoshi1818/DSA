b777c1100bcbe767b1ba2ea8628751918c0fbfc6
Pjoshi1818 <pankajmenariyajoshi@gmail.com>
2025-07-27 10:18:37 +0530

    maximum subarray sum

--- subARRsum.cpp
+++ subARRsum.cpp
@@ -1,20 +1,43 @@
+// #include<iostream>
+// #include<climits>
+// using namespace std;
+// int main(){
+    
+//         int arr[5] = {1,2,3,4,5};
+//         int cs = 0;
+//         int maxcs = INT_MIN;
+//             for(int i= 0; i<5;i++){
+//                 cs = cs+arr[i];
+//                 maxcs = max(cs,maxcs);
+//                 if(cs < 0){
+//                     cs = 0;
+//             }
+//         }
+//         cout<<maxcs<<endl;
+    
+//     return 0;
+     
+// }
+
+
+
 #include<iostream>
 #include<climits>
 using namespace std;
+
+
 int main(){
-    
-        int arr[5] = {1,2,3,4,5};
-        int cs = 0;
-        int maxcs = INT_MIN;
-            for(int i= 0; i<5;i++){
-                cs = cs+arr[i];
-                maxcs = max(cs,maxcs);
-                if(cs < 0){
-                    cs = 0;
+    int n = 5;
+    int arr[n] = {1,2,3,4,5};
+        int maxsum = INT_MIN;
+        for(int st = 0;st<n;st++ ){
+            int cs = 0;
+            for(int end = st ; end <n ;end++){
+                cs = cs+ arr[end];
+                maxsum = max(cs, maxsum);
             }
         }
-        cout<<maxcs<<endl;
-    
+
+cout<<"maxsum is this = "<<maxsum<<endl;
     return 0;
-     
 }
