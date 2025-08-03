2600ac4d5c2c408c1c0f05cebbbff7bd037a8162
Pjoshi1818 <pankajmenariyajoshi@gmail.com>
2025-08-03 09:18:31 +0530

    reverse a string

--- string_reverse_words.cpp
+++ string_reverse_words.cpp
@@ -3,14 +3,24 @@ using namespace std;
 int main(){
 
 
-    string rvworsd = "hello world";
+    string rv = "hello world";
 
     int st = 0;
-    int end = rvworsd.length()-1;
+    int end = rv.length()-1;
 
+
+while(st<end){
+    swap(rv[st],rv[end]);
+    st++;
+    end--;
+    } cout<<rv<<endl;
+for (int i = 0; i < end; i++)
+{
+  
+if(rv[i] == ' ' ){
     
-
-
+}
+}
 
 
     return 0;
