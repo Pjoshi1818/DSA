49e81ab9864a8ddfd03d6f8198d3774c2ca4a4d3
Pjoshi1818 <pankajmenariyajoshi@gmail.com>
2025-09-03 22:13:46 +0530

    anagream

--- MAP/Valid_Anagram.cpp
+++ MAP/Valid_Anagram.cpp
@@ -3,21 +3,18 @@
 using namespace std;
 
 bool isAnagram(string s, string t) {
-    if (s.size() != t.size()) return false;
+  
 
     unordered_map<char, int> freq;
 
-    // Count frequency for string s
     for (char c : s) {
         freq[c]++;
     }
 
-    // Decrease frequency for string t
     for (char c : t) {
         freq[c]--;
     }
 
-    // Check if all counts are zero
     for (auto p : freq) {
         if (p.second != 0) return false;
     }
